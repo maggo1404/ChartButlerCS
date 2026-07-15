b5970f1d8d1d2091726154602fe5dea6420c623c
Marco <509301+maggo1404@users.noreply.github.com>
2026-07-15 08:29:15 +0200

    Refactor Utility.cs with updated code structure

--- ChartButlerCS/Utility.cs
+++ ChartButlerCS/Utility.cs
@@ -208,5 +208,11 @@ namespace ChartButlerCS
             return filename;
         }
 
+        /// <summary>
+        /// User-Agent String für die Webfragen.
+        /// </summary>
+        public const string USER_AGENT =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0";
+
     }
 }
